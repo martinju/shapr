2e60034e077a04e0dffc2d1b00402e89e5cfe53e
Martin Jullum <jullum@nr.no>
2019-10-21 15:48:27 +0200

    Initializing

---
+++ inst/scripts/testing_MRF.R
@@ -0,0 +1,14 @@
+#### Testing MultivariateRandomForest package #####
+
+
+install.packages("MultivariateRandomForest")
+#install.packages("IntegratedMRF")
+
+
+library(MultivariateRandomForest)
+
+#library(IntegratedMRF)
+IntegratedMRF::build_single_tree # Calling MultivariateRandomForest, so no point of look ing at IntegradtedMRF
+
+
+MultivariateRandomForest::build_single_tree
